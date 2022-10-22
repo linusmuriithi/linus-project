1a890ef5c266906f269daef435fb38181fc79b5b
linusmuriithi <78343555+linusmuriithi@users.noreply.github.com>
2022-10-22 14:35:09 +0300

    Delete admin directory

--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <strong>Copyright &copy; 2020 Brought To You By<a href="https://itsourcecode.com/">IT SOURCE CODE</a></strong>
-</footer>
